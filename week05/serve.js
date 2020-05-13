c87aa382f07b9b60c9abeef2e531eecfdc2e022c
apple <apple@appledeMacBook-Air.local>
2020-05-13 16:09:15 +0800

    5

---
+++ week05/serve.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+
+const server = http.createServer((req, res) => {
+    console.log('request received');
+    console.log(req.headers)
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("X-Foo", "bar");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    res.end('jianting');
+});
+
+server.listen("8080");
